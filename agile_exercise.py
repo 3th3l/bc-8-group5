5b7d9486162e140da0132aad056e480d67d3f65c
Hannah Masila <hannahmasila@gmail.com>
2016-06-16 16:54:22 +0300

    adds agile-exercise on maslah branch The Employee class which contains details of an employee which include:- 	-name 	-gender 	number of transactions the track_transaction function which adds the number of transactions made the review_salary function which reviews employee salary depending on the transactions made

--- agile_exercise.py
+++ agile_exercise.py
@@ -1,43 +1,74 @@
 class Employee(object):
-	def __init__(self, name, gender, num_transaction, salary):
-		self.name = name
-		self.gender = gender
-		self.num_transaction = num_transaction
-		self.salary = salary
+    
+    """
+Employee class which receives information about an employee. Details
+should include:- name, gender, number of transaction and salary
+    """
 
-	def track_tran(self, num):
-		self.num_transaction += num
+    def __init__(self, name, gender, num_transaction, salary):
+                
+        self.name = name
+        self.gender = gender
+        self.num_transaction = num_transaction
+        self.salary = salary
 
-	def review_sal(self, num_tran):
-		self.salary *= num_tran/10
+    def track_tran(self, num):
+        """
+Adds the number of transactions made
+        """
+
+        self.num_transaction += num
+
+    def review_sal(self, num_tran):
+        """
+Reviews employee salary depending on the number of transactions
+        """
+
+        self.salary *= num_tran/10
 
 class Inventory(object):
-	def __init__(self, stock_id, stock_name, stock_qty):
-		self.stock_id = stock_id
-		self.stock_name = stock_name
-		self.stock_qty = stock_qty
+    
+    """
+Inventory class that takes details of an inventory. These are:-
+stock id, stock name and stock quantity
+    """
 
-	def dec_stock(self, qty):
-		self.stock_qty -= qty
+    def __init__(self, stock_id, stock_name, stock_qty):
+        
+        self.stock_id = stock_id
+        self.stock_name = stock_name
+        self.stock_qty = stock_qty
 
-	def inc_stock(self, qty):
-		self.stock_qty += qty
+    def dec_stock(self, qty):
+        
+        """
+A function that deducts the quantity when stock has been bought
+        """
 
-emp1 = Employee("Ethel Jackie Hannah", "Female", 0, 50000)
+        self.stock_qty -= qty
 
-inv1 = Inventory(00011, "Omo 1 KG", 33)
+    def inc_stock(self, qty):
+        """
+A functions that adds new stock
+        """
 
-print inv1.stock_qty
-print emp1.num_transaction
+        self.stock_qty += qty
+
+emp1 = Employee("Hannah Maslah", "Female", 1, 50000)
+
+inv1 = Inventory(11, "Omo 1 KG", 33)
+
+print (inv1.stock_qty)
+print (emp1.num_transaction)
 
 #Sell Omo
 inv1.dec_stock(4)
 
-#Update "Ethel Jackie Hannah" transactions
+#Update "Hannah Maslah's" transactions
 emp1.track_tran(1)
 
-print inv1.stock_qty
-print emp1.num_transaction
+print (inv1.stock_qty)
+print (emp1.num_transaction)
 
 
 
