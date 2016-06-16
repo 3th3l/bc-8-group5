bedf274fa4e3a331b8fbef4a6b19faf9ad3922af
Jackie Macharia <jacklinewmacharia@gmail.com>
2016-06-16 21:33:41 +0300

    Modifies agile_exercise.py
    Adds comments to the file and replaces print commands with return

--- agile_exercise.py
+++ agile_exercise.py
@@ -1,34 +1,59 @@
 class Employee(object):
+
+	"""Creates an Employee class """
+
+
 	def __init__(self, name, gender, num_transaction, salary):
+		"""
+		Initializes the Employee class
+		Takes the following attributes: name, gender, num_transaction, salary
+		 """ 
+
+
 		self.name = name
 		self.gender = gender
 		self.num_transaction = num_transaction
 		self.salary = salary
 
+
 	def track_tran(self, num):
+		"""Method that tracks the number of transactions """
 		self.num_transaction += num
 
+
 	def review_sal(self, num_tran):
+		"""Method that reviews salaries based on number of transactions"""
 		self.salary *= num_tran/10
 
+
+
 class Inventory(object):
+	"""Creates an Inventory class"""
+
+
 	def __init__(self, stock_id, stock_name, stock_qty):
+		"""Initializes the Inventory class"""
 		self.stock_id = stock_id
 		self.stock_name = stock_name
 		self.stock_qty = stock_qty
 
+
 	def dec_stock(self, qty):
+		"""Method that tracks stock usage"""
 		self.stock_qty -= qty
 
+
 	def inc_stock(self, qty):
+		"""Method that replenishes stock"""
 		self.stock_qty += qty
+
 
 emp1 = Employee("Ethel Jackie Hannah", "Female", 0, 50000)
 
 inv1 = Inventory(00011, "Omo 1 KG", 33)
 
-print inv1.stock_qty
-print emp1.num_transaction
+return inv1.stock_qty
+return emp1.num_transaction
 
 #Sell Omo
 inv1.dec_stock(4)
@@ -36,8 +61,8 @@ inv1.dec_stock(4)
 #Update "Ethel Jackie Hannah" transactions
 emp1.track_tran(1)
 
-print inv1.stock_qty
-print emp1.num_transaction
+return inv1.stock_qty
+return emp1.num_transaction
 
 
 
